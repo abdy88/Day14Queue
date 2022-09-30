6c66d13e06bd8e2c9f7dd263f9ce3ace2c093f2f
aytech11 <aytech0110@gmail.com>
2022-09-30 19:06:02 +0530

    UC1

--- Main.java
+++ Main.java
@@ -8,10 +8,13 @@ public class Main {
 	public static void main(String[] args) {
 	
 		QueueOperations obj2 = new QueueOperations();
+		
+//		UC1
 		obj2.enqueue(56);
 		obj2.enqueue(30);
 		obj2.enqueue(70);
 		obj2.display1();
+	
 	
 	}
 	
